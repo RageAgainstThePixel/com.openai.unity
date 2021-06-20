5934fb66f0dedac59152185f3838e2f412109e0c
Stephen Hodgson <hodgson.designs@gmail.com>
2021-06-20 02:39:59 -0400

    initial project setup

---
+++ Tests/ExampleTestScript.cs
@@ -0,0 +1,16 @@
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using NUnit.Framework;
+
+namespace OpenAI.Tests
+{
+    internal class ExampleTestScript
+    {
+        [Test]
+        public void ExampleTestScriptSimplePasses()
+        {
+            // A Test behaves as an ordinary method
+            // Use the Assert class to test conditions
+        }
+    }
+}
