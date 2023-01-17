a24f32362db56f4ce1248580a1cf6f69c93575c8
Stephen Hodgson <hodgson.designs@gmail.com>
2023-01-17 12:04:56 -0500

    Added default moderation model

--- Runtime/Moderations/ModerationsEndpoint.cs
+++ Runtime/Moderations/ModerationsEndpoint.cs
@@ -1,6 +1,7 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using Newtonsoft.Json;
+using OpenAI.Models;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -22,14 +23,22 @@ namespace OpenAI.Moderations
             => $"{Api.BaseUrl}moderations";
 
         /// <summary>
-        /// Classifies if text violates OpenAI's Content Policy
+        /// Classifies if text violates OpenAI's Content Policy.
         /// </summary>
+        /// <param name="input">
+        /// The input text to classify.
+        /// </param>
+        /// <param name="model">The default is text-moderation-latest which will be automatically upgraded over time.
+        /// This ensures you are always using our most accurate model.
+        /// If you use text-moderation-stable, we will provide advanced notice before updating the model.
+        /// Accuracy of text-moderation-stable may be slightly lower than for text-moderation-latest.
+        /// </param>
         /// <returns>
         /// True, if the text has been flagged by the model as violating OpenAI's content policy.
         /// </returns>
-        public async Task<bool> GetModerationAsync(string text)
+        public async Task<bool> GetModerationAsync(string input, Model model = null)
         {
-            var result = await CreateModerationAsync(new ModerationsRequest(text));
+            var result = await CreateModerationAsync(new ModerationsRequest(input, model));
 
             if (result?.Results == null ||
                 result.Results.Count == 0)
