6c1fba56e5d5ab30295a47cbdfe98dbebc0088d9
Stephen Hodgson <hodgson.designs@gmail.com>
2023-02-09 22:59:30 -0500

    bump version to 2.2.3
    added OPEN_AI_ORGANIZATION_ID environment variable
    deprecated Organization use OrganizationId instead

--- OpenAI/Packages/com.openai.unity/Runtime/OpenAIClient.cs
+++ OpenAI/Packages/com.openai.unity/Runtime/OpenAIClient.cs
@@ -50,9 +50,9 @@ namespace OpenAI
             Client.DefaultRequestHeaders.Add("User-Agent", "com.openai.unity");
             Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", OpenAIAuthentication.ApiKey);
 
-            if (!string.IsNullOrWhiteSpace(OpenAIAuthentication.Organization))
+            if (!string.IsNullOrWhiteSpace(OpenAIAuthentication.OrganizationId))
             {
-                Client.DefaultRequestHeaders.Add("OpenAI-Organization", OpenAIAuthentication.Organization);
+                Client.DefaultRequestHeaders.Add("OpenAI-Organization", OpenAIAuthentication.OrganizationId);
             }
 
             Version = 1;
