c24b973ffcdfbb446065d79294a0ec2bc37a6434
Stephen Hodgson <hodgson.designs@gmail.com>
2023-03-06 13:25:15 -0500

    added model verification
    added multiple embedding input
    updated unit tests

--- OpenAI/Packages/com.openai.unity/Runtime/Embeddings/EmbeddingsEndpoint.cs
+++ OpenAI/Packages/com.openai.unity/Runtime/Embeddings/EmbeddingsEndpoint.cs
@@ -2,6 +2,7 @@
 
 using Newtonsoft.Json;
 using OpenAI.Models;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -38,6 +39,25 @@ namespace OpenAI.Embeddings
         /// <returns><see cref="EmbeddingsResponse"/></returns>
         public async Task<EmbeddingsResponse> CreateEmbeddingAsync(string input, Model model = null, string user = null)
             => await CreateEmbeddingAsync(new EmbeddingsRequest(input, model, user));
+
+        /// <summary>
+        /// Creates an embedding vector representing the input text.
+        /// </summary>
+        /// <param name="input">
+        /// Input text to get embeddings for, encoded as a string or array of tokens.
+        /// To get embeddings for multiple inputs in a single request, pass an array of strings or array of token arrays.
+        /// Each input must not exceed 8192 tokens in length.
+        /// </param>
+        /// <param name="model">
+        /// ID of the model to use.
+        /// Defaults to: text-embedding-ada-002
+        /// </param>
+        /// <param name="user">
+        /// A unique identifier representing your end-user, which can help OpenAI to monitor and detect abuse.
+        /// </param>
+        /// <returns><see cref="EmbeddingsResponse"/></returns>
+        public async Task<EmbeddingsResponse> CreateEmbeddingAsync(IEnumerable<string> input, Model model = null, string user = null)
+            => await CreateEmbeddingAsync(new EmbeddingsRequest(input, model, user)).ConfigureAwait(false);
 
         /// <summary>
         /// Creates an embedding vector representing the input text.
