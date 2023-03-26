35730b3a3d7c7da2d38193c8ef1eaedf03bdfbea
Stephen Hodgson <hodgson.designs@gmail.com>
2023-03-26 15:26:43 -0400

    marked more classes obsolete
    updated docs

--- OpenAI/Packages/com.openai.unity/Runtime/Edits/Choice.cs
+++ OpenAI/Packages/com.openai.unity/Runtime/Edits/Choice.cs
@@ -1,9 +1,11 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using System;
 using Newtonsoft.Json;
 
 namespace OpenAI.Edits
 {
+    [Obsolete]
     public sealed class Choice
     {
         [JsonConstructor]
--- OpenAI/Packages/com.openai.unity/Runtime/Edits/EditResponse.cs
+++ OpenAI/Packages/com.openai.unity/Runtime/Edits/EditResponse.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 
 namespace OpenAI.Edits
 {
+    [Obsolete]
     public sealed class EditResponse : BaseResponse
     {
         [JsonConstructor]
