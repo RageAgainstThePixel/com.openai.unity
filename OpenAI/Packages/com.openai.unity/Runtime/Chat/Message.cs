c6ae1f1d983b4c446a5ebbdee4fef7165edb7c1a
Stephen Hodgson <hodgson.designs@gmail.com>
2023-04-23 15:30:29 -0400

    - Added optional author name property to chat message
    - Added implicit string conversions to make ChatResponses easier to work with

--- OpenAI/Packages/com.openai.unity/Runtime/Chat/Message.cs
+++ OpenAI/Packages/com.openai.unity/Runtime/Chat/Message.cs
@@ -9,31 +9,36 @@ namespace OpenAI.Chat
     [Serializable]
     public sealed class Message
     {
-        [Obsolete("Use new constructor with enum Role")]
-        public Message(string role, string content)
-        {
-            this.role = role.ToLower() switch
-            {
-                "system" => Role.System,
-                "assistant" => Role.Assistant,
-                "user" => Role.User,
-                _ => throw new ArgumentException(nameof(role))
-            };
-            this.content = content;
-        }
-
+        /// <summary>
+        /// Creates a new message to insert into a chat conversation.
+        /// </summary>
+        /// <param name="role">
+        /// The <see cref="Chat.Role"/> of the author of this message.
+        /// </param>
+        /// <param name="content">
+        /// The contents of the message.
+        /// </param>
+        /// <param name="name">
+        /// Optional, The name of the author of this message.<br/>
+        /// May contain a-z, A-Z, 0-9, and underscores, with a maximum length of 64 characters.
+        /// </param>
         [JsonConstructor]
         public Message(
             [JsonProperty("role")] Role role,
-            [JsonProperty("content")] string content)
+            [JsonProperty("content")] string content,
+            [JsonProperty("name")] string name = null)
         {
             this.role = role;
             this.content = content;
+            this.name = name;
         }
 
         [SerializeField]
         private Role role;
 
+        /// <summary>
+        /// The <see cref="Chat.Role"/> of the author of this message.
+        /// </summary>
         [JsonProperty("role")]
         public Role Role => role;
 
@@ -41,9 +46,24 @@ namespace OpenAI.Chat
         [TextArea(1, 30)]
         private string content;
 
+        /// <summary>
+        /// The contents of the message.
+        /// </summary>
         [JsonProperty("content")]
         public string Content => content;
 
-        public static implicit operator string(Message message) => message.Content;
+        [SerializeField]
+        private string name;
+
+        /// <summary>
+        /// Optional, The name of the author of this message.<br/>
+        /// May contain a-z, A-Z, 0-9, and underscores, with a maximum length of 64 characters.
+        /// </summary>
+        [JsonProperty("name")]
+        public string Name => name;
+
+        public override string ToString() => Content;
+
+        public static implicit operator string(Message message) => message.ToString();
     }
 }
