dd4042459e394e992050dd993fd6e2c1ced6936d
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2025-04-27 13:24:32 -0400

    updated audio models list

--- OpenAI/Packages/com.openai.unity/Runtime/Realtime/InputAudioBufferStartedResponse.cs
+++ OpenAI/Packages/com.openai.unity/Runtime/Realtime/InputAudioBufferStartedResponse.cs
@@ -5,15 +5,23 @@ using UnityEngine.Scripting;
 
 namespace OpenAI.Realtime
 {
+    /// <summary>
+    /// Sent by the server when in server_vad mode to indicate that speech has been detected in the audio buffer.
+    /// This can happen any time audio is added to the buffer (unless speech is already detected).
+    /// The client may want to use this event to interrupt audio playback or provide visual feedback to the user.
+    /// The client should expect to receive a input_audio_buffer.speech_stopped event when speech stops.
+    /// The item_id property is the ID of the user message item that will be created when speech stops and
+    /// will also be included in the input_audio_buffer.speech_stopped event (unless the client manually commits the audio buffer during VAD activation).
+    /// </summary>
     [Preserve]
     public sealed class InputAudioBufferStartedResponse : BaseRealtimeEvent, IServerEvent
     {
         [Preserve]
         [JsonConstructor]
         internal InputAudioBufferStartedResponse(
-            [JsonProperty("event_id")] string eventId,
-            [JsonProperty("type")] string type,
-            [JsonProperty("audio_start_ms")] int audioStartMs,
+        [JsonProperty("event_id")] string eventId,
+        [JsonProperty("type")] string type,
+        [JsonProperty("audio_start_ms")] int audioStartMs,
             [JsonProperty("item_id")] string itemId)
         {
             EventId = eventId;
