0b0cf6a4380d2338f997507e9c8dae8c467ff014
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2025-05-23 14:06:34 -0400

    broke out some files
    updated icons

---
+++ OpenAI/Packages/com.openai.unity/Runtime/Realtime/VAD_Eagerness.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.Serialization;
+using UnityEngine.Scripting;
+
+namespace OpenAI.Realtime
+{
+    [Preserve]
+    public enum VAD_Eagerness
+    {
+        [EnumMember(Value = "auto")]
+        Auto = 0,
+        [EnumMember(Value = "low")]
+        Low,
+        [EnumMember(Value = "medium")]
+        Medium,
+        [EnumMember(Value = "high")]
+        High
+    }
+}
