a81f08668f784cad1b7bd013434700fc2cb4c85b
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2025-07-04 16:50:27 -0400

    com.openai.unity 8.8.1 (#394)
    - updated realtime audio transcription settings properties
    - bumped deps
    
    ---------
    
    Co-authored-by: AG3D <andries@pixelbricks.be>
    Co-authored-by: Andries Geens <andries.geens@gmail.com>

--- OpenAI/Packages/com.openai.unity/Runtime/Realtime/InputAudioTranscriptionSettings.cs
+++ OpenAI/Packages/com.openai.unity/Runtime/Realtime/InputAudioTranscriptionSettings.cs
@@ -2,18 +2,43 @@
 
 using Newtonsoft.Json;
 using OpenAI.Models;
+using UnityEngine.Scripting;
 
 namespace OpenAI.Realtime
 {
+    [Preserve]
     public sealed class InputAudioTranscriptionSettings
     {
-        [JsonConstructor]
-        public InputAudioTranscriptionSettings([JsonProperty("model")] Model model)
+        [Preserve]
+        public InputAudioTranscriptionSettings(Model model, string prompt = null, string language = null)
         {
-            Model = string.IsNullOrWhiteSpace(model.Id) ? "whisper-1" : model;
+            Model = string.IsNullOrWhiteSpace(model?.Id) ? Models.Model.Whisper1 : model;
+            Prompt = prompt;
+            Language = language;
         }
 
+        [Preserve]
+        [JsonConstructor]
+        internal InputAudioTranscriptionSettings(
+            [JsonProperty("model")] string model,
+            [JsonProperty("prompt")] string prompt = null,
+            [JsonProperty("language")] string language = null)
+        {
+            Model = model;
+            Prompt = prompt;
+            Language = language;
+        }
+
+        [Preserve]
         [JsonProperty("model")]
         public string Model { get; }
+
+        [Preserve]
+        [JsonProperty("prompt")]
+        public string Prompt { get; }
+
+        [Preserve]
+        [JsonProperty("language")]
+        public string Language { get; }
     }
 }
