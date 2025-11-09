316efef68f1b322071ed8c1eef188c8cfc6a5bf7
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2025-11-09 13:49:17 -0500

    com.openai.unity 8.8.7 (#431)
    - Fix VAD serialization not properly setting disabled values

--- OpenAI/Packages/com.openai.unity/Runtime/Realtime/IVoiceActivityDetectionSettings.cs
+++ OpenAI/Packages/com.openai.unity/Runtime/Realtime/IVoiceActivityDetectionSettings.cs
@@ -6,7 +6,7 @@ namespace OpenAI.Realtime
     public interface IVoiceActivityDetectionSettings
     {
         public TurnDetectionType Type { get; }
-        public bool CreateResponse { get; }
-        public bool InterruptResponse { get; }
+        public bool? CreateResponse { get; }
+        public bool? InterruptResponse { get; }
     }
 }
